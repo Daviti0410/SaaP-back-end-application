0d4eada9d1a03cf6cef95ec5373b9f3993f41aca
Daviti_Lazariashvili <lazariashvilidato@gmail.com>
2024-03-12 18:09:04 +0400

    validation changes

--- src/validators/authValidators/index.js
+++ src/validators/authValidators/index.js
@@ -5,10 +5,9 @@ const validateRegister = async (req, res, next) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
-    firstName: Joi.string().min(2).required(),
-    lastName: Joi.string().min(2).required(),
-    balance: Joi.number().min(0).required(),
-    userType: Joi.number().required()
+    companyName: Joi.string().min(2).required(),
+    industry: Joi.string().min(2).required(),
+    country: Joi.number().min(0).required(),
   });
 
   return joiValidator({ schema, objectToValidate: req.body, next })
